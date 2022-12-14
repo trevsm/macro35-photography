832f2e8a0ce45aeba1f58e799cbe851d29ae38c7
Trevor Smith <trevorsm@pm.me>
2022-12-14 13:58:08 -0700

    footer

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,15 +1,98 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Content } from "../styles";
+import { routes } from "../routes";
+import { useMemo } from "react";
+import Spacer from "./Spacer";
 
 const StyledFooter = styled.footer`
-  background: lightgreen;
+  background: #282828;
   width: 100%;
+  * {
+    color: white;
+  }
+  .content {
+    position: relative;
+    padding: 5rem 0;
+    padding-bottom: 8rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  h4 {
+    border-bottom: 1px solid white;
+    width: fit-content;
+    text-transform: uppercase;
+  }
+  .links {
+    a {
+      text-decoration: none;
+      margin-bottom: 0.5rem;
+    }
+    display: flex;
+    flex-direction: column;
+  }
+  .copyright,
+  .logo {
+    position: absolute;
+  }
+
+  .copyright {
+    bottom: 0;
+    padding: 2rem;
+  }
+
+  .logo {
+    top: 0;
+    left: 0;
+    padding: 2rem;
+  }
 `;
 
 export default function Footer() {
+  const routeLinks = useMemo(
+    () =>
+      routes.map((route) =>
+        route.name ? (
+          <Link key={route.path} to={route.path}>
+            {route.name}
+          </Link>
+        ) : null
+      ),
+    [routes]
+  );
   return (
     <StyledFooter>
-      <Content>Footer</Content>
+      <Content>
+        <Spacer />
+        <div className="logo">MACRO35</div>
+        <div className="links">
+          <h4>Links</h4>
+          {routeLinks}
+        </div>
+        <div className="right">
+          <div className="location">
+            <h4>Studio Location</h4>
+            <p>
+              123 Glover Lane <br />
+              Miniville, UT 83015 <br />
+              <a href="https://goo.gl/maps"> View on Map</a>
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <h4>Contact</h4>
+          <p>
+            Tel: +1 (801) 901-2345 <br />
+            Fax: +1 (801) 901-2345 <br />
+            Email: hello@macro35.com <br />
+          </p>
+        </div>
+        <div className="copyright">
+          &copy; {new Date().getFullYear()} Macro35. All rights reserved.
+        </div>
+        <Spacer />
+      </Content>
     </StyledFooter>
   );
 }
